a92c67b0c457b07ca57a32b0b4be482dbb4f935f
jexzyjames <jexzyjames@gmail.com>
2025-05-26 00:20:17 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import TodoContainer from './components/TodoContainer'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import TodoContainer from "./components/TodoContainer";
 
 function App() {
-  const[theme, setTheme] = useState('light')
-useEffect(()=>{
-  const savedTheme = localStorage.getItem('theme');
-  if(savedTheme) {
-    setTheme(savedTheme);
-  } else {
-    localStorage.setItem('theme', 'light');
-  }
-}
-, []);
+  const [theme, setTheme] = useState("light");
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  }, []);
   return (
-    <div className=' '>
-    <Header theme={theme} setTheme={setTheme}/>
-    <div className={`bg-[#fafafa] fadas ${theme === 'light' ? 'bg-[#fafafa] fades' : 'bg-[hsl(235,_21%,_11%)] fades'} w-full min-h-screen  dark:bg-[#161722] dark:text-white`}>
-
-    <TodoContainer theme={theme} setTheme={setTheme} />
+    <div className=" ">
+      <Header theme={theme} setTheme={setTheme} />
+      <div
+        className={`bg-[#fafafa] fadas ${
+          theme === "light"
+            ? "bg-[#fafafa] fades"
+            : "bg-[hsl(235,_21%,_11%)] fades"
+        } w-full min-h-screen  dark:bg-[#161722] dark:text-white`}
+      >
+        <TodoContainer theme={theme} setTheme={setTheme} />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,44 @@
-import React,{useState, useEffect} from 'react'
-import bg from '../assets/images/bg-desktop-light.jpg'
-import mobilebg from '../assets/images/bg-mobile-light.jpg'
-import mobiledarkbg from '../assets/images/bg-mobile-dark.jpg'
-import darkbg from '../assets/images/bg-desktop-dark.jpg'
-import moon from '../assets/images/icon-moon.svg'
-import sun from '../assets/images/icon-sun.svg'
-const Header = ({theme, setTheme}) => {
-
+import React, { useState, useEffect } from "react";
+import bg from "../assets/images/bg-desktop-light.jpg";
+import mobilebg from "../assets/images/bg-mobile-light.jpg";
+import mobiledarkbg from "../assets/images/bg-mobile-dark.jpg";
+import darkbg from "../assets/images/bg-desktop-dark.jpg";
+import moon from "../assets/images/icon-moon.svg";
+import sun from "../assets/images/icon-sun.svg";
+const Header = ({ theme, setTheme }) => {
   return (
-    <div className='header  relative mx-auto  ml-auto w-full '>
-
-      {theme === 'light' ?
-      <>
-      <img className='hidden md:block w-full h-50' src={bg} alt="" />
-      <img className='md:hidden w-full ' src={mobilebg} alt="" />
-      </>
-      :
-      <>
-      <img className='hidden w-full h-50 md:block' src={darkbg} alt=""  />
-      <img className='md:hidden w-full' src={mobiledarkbg} alt="" />
-      </>
-
-      
-    }
-      <div className='absolute flex justify-between md:justify-center  left-0 right-0 max-w-sm md:max-w-xs mx-auto md:gap-70 items-center md:top-10 w-full  top-10 text-white text-2xl font-extrabold'>
-        <h1 className='text-3xl tracking-widest '>TODO</h1>
-        {theme === 'light' ?
-        
-        <img src={moon} alt="moon" className='ml-4 cursor-pointer' onClick={() => setTheme('dark')} />
-        :
-        <img src={sun} alt="sun" className='ml-4 cursor-pointer' onClick={() => setTheme('light')} />
-        }
-        </div>
+    <div className="header  relative mx-auto  ml-auto w-full ">
+      {theme === "light" ? (
+        <>
+          <img className="hidden md:block w-full h-50" src={bg} alt="" />
+          <img className="md:hidden w-full h-50 " src={mobilebg} alt="" />
+        </>
+      ) : (
+        <>
+          <img className="hidden w-full h-50 md:block" src={darkbg} alt="" />
+          <img className="md:hidden w-full h-50" src={mobiledarkbg} alt="" />
+        </>
+      )}
+      <div className="absolute flex justify-between md:justify-center  left-0 right-0 max-w-sm md:max-w-xs mx-auto md:gap-70 items-center md:top-10 w-full  top-10 text-white text-2xl font-extrabold">
+        <h1 className="text-3xl tracking-widest ">TODO</h1>
+        {theme === "light" ? (
+          <img
+            src={moon}
+            alt="moon"
+            className="ml-4 cursor-pointer"
+            onClick={() => setTheme("dark")}
+          />
+        ) : (
+          <img
+            src={sun}
+            alt="sun"
+            className="ml-4 cursor-pointer"
+            onClick={() => setTheme("light")}
+          />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
