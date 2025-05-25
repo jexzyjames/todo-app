4bf3d140cdd4dc83aabd863eaedd3ad96d6d8c3c
jexzyjames <jexzyjames@gmail.com>
2025-05-26 00:20:04 +0100

    n

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -4,38 +4,48 @@ import cross from "../assets/images/icon-cross.svg";
 import { useState } from "react";
 const TodoContainer = ({theme}) => {
   const [todo, setTodo] = useState("");
-  const[newTodo, setMewTodo] = useState(todo);
 
-  const [pages, setPages] = useState("all"); // State to manage the current page (all, active, completed)
+  const [pages, setPages] = useState("all"); 
   const [todos, setTodos] = useState([
     { id: 1, text: "Complete the project", completed: false },
     { id: 2, text: "Read a book", completed: true },
     { id: 3, text: "Go for a walk", completed: false },
   ]);
+  useEffect(() => {
+    const storedTodos = localStorage.getItem('TODOS');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));   
+    }
+    else {
+      localStorage.setItem('TODOS', JSON.stringify(todos));
+    }
+
+
+  }, []);
   
   useEffect(()=>{
-    localStorage.setItem('TODOS', JSON.stringify(todo))
+    localStorage.setItem('TODOS', JSON.stringify(todos))
   },[todos, theme])
   const handleAddTodo = () => {
-    if (todo.trim() === "") return; // Prevent adding empty todos
+    if (todo.trim() === "") return; 
     const newTodo = {
-      id: Date.now(), // Unique ID based on timestamp
+      id: Date.now(), 
       text: todo,
       completed: false,
     };
     if (todos.some((t) => t.text === newTodo.text)) {
       alert("Todo already exists");
-      return; // Prevent adding duplicate todos
+      return; 
     }
     setTodos([...todos, newTodo]);
-    setTodo(""); // Clear input field after adding
-      localStorage.setItem('TODOS', JSON.stringify(todo));
+    localStorage.setItem('TODOS', JSON.stringify(todo));
+    setTodo(""); 
 
   };
   return (
     <div className="flex fades flex-col mt-[-90px] justify-center md:mt-[-90px]  z-1000 relative w-full md:p-0 mx-auto  max-w-lg items-center md:mx-auto">
       <div className={`fades ${theme === 'light' ? 'bg-white fades !text-black' : 'fades bg-[hsl(235,_24%,_19%)] !text-white'} fades  relative w-[90%] shadow-md mb-4 md:w-full md:!mx-[26px] md:max-w-[450px] rounded-sm flex justify-start gap-4 px-4  py-3`}>
-        <input className="" type="radio" name="todo" />
+        <input className="" checked={false} type="radio" name="todo" />
         <input
           className=" border-0 outline-0 "
           onKeyDown={(event) => {
@@ -50,14 +60,21 @@ const TodoContainer = ({theme}) => {
         />
       </div>
 
-      <div className="md:w-full w-[90%]  mx-auto flex justify-center items-center flex-col gap-2 md:max-w-[450px]">
+      <div className="md:w-full w-[90%]  mx-auto flex justify-center items-center flex-col md:max-w-[450px]">
         {todos.length > 0 && todos.map((todo) => (
-          <div className={`w-full md:mx-auto fades  ${theme === 'light' ? '!bg-white fades text-black ' : 'text-white fades bg-[hsl(235,_24%,_19%)]'}  `} key={todo.id}>
+          <div className={`w-full  md:mx-auto fades  ${theme === 'light' ? '!bg-white border border-t-0 border-r-0 border-l-0 border-b-[#999] fades text-black ' : 'text-white  border-[1px]  border-b-[#fafafa28] border-t-0 border-r-0 border-l-0 fades bg-[hsl(235,_24%,_19%)]'}  `} key={todo.id}>
 
             {pages === "active" && !todo.completed && (
               <div
                 key={todo.id}
-                className=" shadow-lg  flex justify-start gap-2 items-center px-4 py-2 "
+                onClick={()=>{
+                  setTodos(
+                    todos.map((t) =>
+                      t.id === todo.id ? { ...t, completed: !t.completed } : t
+                    )
+                  ); 
+                }}
+                className=" rounded-sm flex justify-start gap-4 items-center px-4 py-3 mt-1 "
               >
                 <input type="radio" checked={todo.completed} className='cursor-pointer' />
                 {todo.completed && (
@@ -80,7 +97,7 @@ const TodoContainer = ({theme}) => {
                     // setTodos([])
                     setTodos(todos.filter((t) => t.id !== todo.id)); 
                     
-                      localStorage.setItem('TODOS', JSON.stringify(newTodo));
+                      localStorage.setItem('TODOS', JSON.stringify(todos));
 
                
                   }}
@@ -93,9 +110,9 @@ const TodoContainer = ({theme}) => {
             {pages === "completed" && todo.completed && (
               <div
                 key={todo.id}
-                className=" shadow-lg rounded-sm flex justify-start gap-4 items-center px-4 py-3 mt-2"
+                className=" rounded-sm flex justify-start gap-4 items-center px-4 py-3 mt-1"
               >
-                {/* <input type="radio" checked={todo.completed} className='cursor-pointer' /> */}
+               {todo.completed === 'false' && <input type="radio" checked={todo.completed} className='cursor-pointer' />}
                 {todo.completed && (
                   <img
                     className="rounded-full p-1 bg-[linear-gradient(to_right,_hsl(192,_100%,_67%),_hsl(280,_87%,_65%))]"
@@ -127,7 +144,7 @@ const TodoContainer = ({theme}) => {
                 key={todo.id}
                 className="  rounded-sm flex justify-start gap-4 items-center px-4 py-3 mt-1"
               >
-                {/* <input type="radio" checked={todo.completed} className='cursor-pointer' /> */}
+               {todo.completed === 'false' || !todo.completed && <input type="radio" checked={todo.completed} className='cursor-pointer' />}
                 {todo.completed && (
                   <img
                     className="rounded-full p-1 bg-[linear-gradient(to_right,_hsl(192,_100%,_67%),_hsl(280,_87%,_65%))]"
@@ -141,7 +158,7 @@ const TodoContainer = ({theme}) => {
                     todos.map((t) =>
                       t.id === todo.id ? { ...t, completed: !t.completed } : t
                     )
-                  ); // Toggle the completed state of the todo
+                  ); 
                 }}
                   className={` cursor-pointer${
                     todo.completed ? " italic line-through  cursor-pointer text-gray-400" : " cursor-pointer"
@@ -153,7 +170,7 @@ const TodoContainer = ({theme}) => {
                 <img
                   src={cross}
                    onClick={()=> {
-                    setTodos(todos.filter((t) => t.id !== todo.id)); // Remove the todo
+                    setTodos(todos.filter((t) => t.id !== todo.id));
 
                   }}
                   alt="delete"
@@ -179,26 +196,26 @@ const TodoContainer = ({theme}) => {
           className={`md:flex hidden     p-2 gap-2 font-semibold text-gray-400 justify-between`}
         >
           <p
-          className={`${pages === "all" ? "text-blue-500 " : "cursor-pointer"}`} // Highlight 'active' page
+          className={` cursor-pointer ${pages === "all" ? "text-blue-500 cursor-pointer" : "cursor-pointer"}`} // Highlight 'active' page
 
             onClick={() => {
-              setPages("all"); // Set the current page to 'all'
+              setPages("all"); 
             }}
           >
             All
           </p>
           <p
-          className={`${pages === "active" ? "text-blue-500" : "cursor-pointer"}`} // Highlight 'active' page
+          className={` cursor-pointer ${pages === "active" ? "text-blue-500 cursor-pointer" : "cursor-pointer"}`} // Highlight 'active' page
             onClick={() => {
-              setPages("active"); // Set the current page to 'active'
+              setPages("active"); 
             }}
           >
             Active
           </p>
           <p
-          className={`${pages === "completed" ? "text-blue-500" : "cursor-pointer"}`} // Highlight 'active' page
+          className={`cursor-pointer ${pages === "completed" ? "text-blue-500 cursor-pointer" : "cursor-pointer"}`} // Highlight 'active' page
             onClick={() => {
-              setPages("completed"); // Set the current page to 'completed'
+              setPages("completed"); 
             }}
           >
             Completed
@@ -215,26 +232,26 @@ const TodoContainer = ({theme}) => {
           className={`flex w-full md:hidden mt-5 ${theme === 'light' ? 'bg-white' : 'bg-[hsl(235,_24%,_19%)] text-white'}   rounded-sm shadow-sm p-2 gap-4 font-semibold text-gray-400 justify-center`}
         >
           <p
-          className={`${pages === "all" ? "text-blue-500" : ""}`} // Highlight 'active' page
+          className={`cursor-pointer ${pages === "all" ? "text-blue-500" : ""}`}
 
             onClick={() => {
-              setPages("all"); // Set the current page to 'all'
+              setPages("all"); 
             }}
           >
             All
           </p>
           <p
-          className={`${pages === "active" ? "text-blue-500" : ""}`} // Highlight 'active' page
+          className={` cursor-pointer ${pages === "active" ? "text-blue-500" : ""}`}
             onClick={() => {
-              setPages("active"); // Set the current page to 'active'
+              setPages("active"); 
             }}
           >
             Active
           </p>
           <p
-          className={`${pages === "completed" ? "text-blue-500" : ""}`} // Highlight 'active' page
+          className={`cursor-pointer ${pages === "completed" ? "text-blue-500" : ""}`} 
             onClick={() => {
-              setPages("completed"); // Set the current page to 'completed'
+              setPages("completed"); 
             }}
           >
             Completed
